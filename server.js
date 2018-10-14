c0f67df4a3e763bcb521333daba0572c1b9240ea
Ashish Singh <ashishcodes4@gmail.com>
2018-10-14 21:00:13 +0530

    adding bootstarp, view engine, templating engine

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// Handlebars Middleware
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+//connect to mongoose
+mongoose.Promise = global.Promise;
 
+mongoose
+  .connect(
+    'mongodb://localhost/ideajot-dev',
+    { useMongoClient: true }
+  )
+  .then(() => {
+    console.log('mongoDB connected');
+  })
+  .catch(err => {
+    console.log('trouble connected mongoDB');
+  });
+
+// Handlebars Middleware
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+  })
+);
+app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
   res.render('home', {
-    title: title
+    title: title,
   });
 });
 
@@ -23,8 +41,7 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
