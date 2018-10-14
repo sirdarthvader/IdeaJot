c1b71ba74b754efe052aca2780fb54caf66db369
Ashish Singh <ashishcodes4@gmail.com>
2018-10-14 19:50:24 +0530

    adding new files in the system

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const PORT =  process.env.PORT || 5000;
 
+// Server middleware
+// app.use()
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.send('root route');
+    res.render('home');
 })
 app.get('/about', (req, res) => {
     res.send('about page');
