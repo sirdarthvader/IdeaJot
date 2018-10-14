5ccb430640a9c1bb259fbd73a081103ee7494f3e
Ashish Singh <ashishcodes4@gmail.com>
2018-10-14 13:42:23 +0530

    added nodemon as server helper

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT =  process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('root route');
 })
-
+app.get('/about', (req, res) => {
+    res.send('about page');
+})
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
 })
